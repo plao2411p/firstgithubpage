0f6d159f22f9d8658c7cdab2919bb1d668af6f3c
Pedro Luis Artunduaga Ortiz <61562230+plao2411p@users.noreply.github.com>
2021-08-03 17:29:19 -0500

    Js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
+
+var matrix = math.matrix([[7, 1],[2, 5]])
+
 function getstrin() {
     let x = document.getElementById("operation").value;
-    alert(typeof (x))
-    document.getElementById("salida").innerHTML = x;
+    depuradorstring(x);
+
+}
+function depuradorstring(operation) {
+    document.getElementById("salida").innerHTML = operation;
+    alert(matrix)
+    function NoNum(cadena) {
+
+    }
+
+    function ConOperation(cadena) {
+
+    }
+
+    function CheckBraques() {
+
+    }
+
+    function CheckOperations() {
+
+    }
 
 }
