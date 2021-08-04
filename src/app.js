377d9d49d4d144a733bf1223fb9541c2737b36a5
Pedro Luis Artunduaga Ortiz <61562230+plao2411p@users.noreply.github.com>
2021-08-03 23:52:28 -0500

    App strucuture

---
+++ src/app.js
@@ -0,0 +1,12 @@
+var operators = "unindids";
+var sets = "UABC";
+
+function getstrin() {
+    let x = document.getElementById("operation").value;
+    depuradorstring(x);
+
+}
+function depuradorstring(operation) {
+    document.getElementById("salida").innerHTML = operation;
+
+}
